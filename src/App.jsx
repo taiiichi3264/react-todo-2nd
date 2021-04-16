22d6f1720fd8a0355290417fd9109ac924b67169
Taiiichi <43616401+taiiichi3264@users.noreply.github.com>
2021-04-16 23:39:40 +0900

    jsxを整形

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,40 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return null;
+  return (
+    <>
+      <div>
+        <input placeholder="todoを入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のtodo</p>
+        <ul>
+          <div>
+            <li>aaaa</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+          <div>
+            <li>aaaa</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+      <div>
+        <p>完了のtodo</p>
+        <ul>
+          <div>
+            <li>aaaa</li>
+            <button>戻す</button>
+          </div>
+          <div>
+            <li>aaaa</li>
+            <button>戻す</button>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
 };
